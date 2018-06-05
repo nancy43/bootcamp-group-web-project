753bd2785757da62c9deaa4c581a39437175cac7
Nancy Sharma <>
2018-06-05 11:36:36 -0400

    added my marvel api

--- js/marvel.js
+++ js/marvel.js
@@ -1,5 +1,6 @@
 var marvel = {
 render: function() {
+    var url = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=b89508034bfeab38dfa4d135c6c96e26&hash=d3a36f12dcdbe4c6fb5f01f854adba63"
 var message = document.getElementById("message");
 $.ajax({
     url: url,
