27fd55b6231e59c9a765b0744e7cf071da686e92
Arsh <boparai.arsh3@gmail.com>
2018-06-05 20:49:09 -0400

    weather of any location

---
+++ js/location.js
@@ -0,0 +1,52 @@
+function getlocation() {
+    var x = document.getElementById("latitude").value;
+    var y = document.getElementById("longitude").value;
+    console.log(x);
+    day = new Date()
+    var img = "leaf"
+    var url = 'https://api.forecast.io/forecast/df7e69b54ab5704912f0b629815f2095/';
+    continue;
+    $.getJSON(url + x + "," + y + "?callback=?", function(data) {
+        var icon = data.currently.icon;
+        $('#temp').html((data.currently.temperature) + '°F');
+        $('#wind').html('Wind Speed: ' + (data.currently.windSpeed) + ' m/h');
+        $('#minutely').html(data.minutely.summary);
+        $('#humidity').html('Humidity: ' + (data.currently.humidity * 100) + '%');
+        $('#precip').html('Precipitation: ' + data.currently.precipIntensity + 'cm');
+        $('#timezone').html(data.timezone);
+        $('#day').html(day);
+        img = newFunction(icon, img);
+        $('#img').html("<img src=images/" + img + ".png></img>");
+        location.innerHTML = data.daily.summary;
+
+
+    });
+
+    function newFunction(icon, img) {
+        if (icon == 'snow') {
+            img = "snow-50";
+        } else if (icon == 'rain') {
+            img = "rain-50";
+        } else if (icon === 'partly-cloudy-day') {
+            img = "partly-cloudy-day-50";
+        } else if (icon == 'partly-cloudy-night') {
+            img = "night-filled-50";
+        } else if (icon == 'clear-day') {
+            img = "sun-50";
+        } else if (icon == 'clear-night') {
+            img = "new-moon-50";
+        } else if (icon == 'sleet') {
+            img = "sleet-50";
+        } else if (icon == 'wind') {
+            img = "windy-weather-50";
+        } else if (icon == 'fog') {
+            img = "dust-50";
+        } else if (icon == 'cloudy') {
+            img = "cloud-50";
+        } else {
+            img = "leaf";
+        }
+        return img;
+    }
+
+}
