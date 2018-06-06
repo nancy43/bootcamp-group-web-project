4609e2e5f778d0efbdb303c3ebe23a72587defa3
bmarchete <bmarchete@gmail.com>
2018-06-06 11:23:11 -0400

    Extracts fetch methods to a new class

--- joao/js/app.js
+++ joao/js/app.js
@@ -2,18 +2,16 @@
 class Guess{
     constructor() {
         
-        this.props = {
-            apiGenderize: 'https://api.genderize.io',
-            apiRestContries: 'https://restcountries.eu/rest/v2/alpha',
-        }
-        
+       
+        // initializes class to fetch data from APIs
+        this.fetch = new FetchAPIs();
         // initializes DOM elements and event handlers
         this.initDOMElements();
 
         // retrieves country data from api and fill country-select
-        this.getContriesData().then(data => {
-            this.props.countries = data;
-            this.fillCountrySelect(this.props.countries);
+        this.fetch.getContriesData().then(data => {
+            
+            this.fillCountrySelect(data);
             
         });
 
@@ -40,33 +38,6 @@ class Guess{
         console.log(this.countrySelect.value);
     }
 
-    // fetch all valid countries codes from genderize
-    async fetchCountriesCodes(){
-        const data = await fetch(`${this.props.apiGenderize}/countries`);
-        return data.json();
-    }
-
-    //fetch all country names given a list of codes
-    async fetchCountriesNames(codes){
-        const data = await fetch(`${this.props.apiRestContries}?codes=${codes}`);
-        return data.json();
-    }
-
-    async getCountriesCode(){
-        const codes = await this.fetchCountriesCodes();
-        return codes.countries.join(';');
-    }
-
-    async getContriesData(){
-        const codes = await this.getCountriesCode();
-        const data = await this.fetchCountriesNames(codes);
-        
-        return data
-            .filter(d => d !== null)
-            .map(value =>  { 
-                return {code: value.alpha2Code, name: value.name}
-            });
-    }
 }
 
 window.onload = () => {
