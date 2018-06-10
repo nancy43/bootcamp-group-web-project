6292729d1819cfa1a570ce46fdef64ddeab7d01a
Nancy Sharma <>
2018-06-10 14:51:47 -0400

    few changes

--- js/marvel.js
+++ js/marvel.js
@@ -3,7 +3,8 @@ var marvel = {
 		var url = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=b89508034bfeab38dfa4d135c6c96e26&hash=d3a36f12dcdbe4c6fb5f01f854adba63"
 		var message = document.getElementById("message");
         var footer = document.getElementById("footer");
-        var marvelContainer = document.getElementById("marvel-container"); 
+		var marvelContainer = document.getElementById("marvel-container"); 
+		
 	  $.ajax({
 		url: url,
 		type:"GET",
