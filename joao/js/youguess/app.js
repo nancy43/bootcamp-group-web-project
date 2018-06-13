48b5d5d6f62a3a412d38748c71d4b30fc2ab4234
bmarchete <bmarchete@gmail.com>
2018-06-13 01:12:51 -0400

    Creates you guess page elements. Changes colors theme

--- joao/js/youguess/app.js
+++ joao/js/youguess/app.js
@@ -1,3 +1,15 @@
 class YouGuess{
-    
+    constructor() {
+        
+        // initializes page elements
+        //this.createDOMElements();
+
+        // initializes DOM elements and event handlers
+        this.initDOMElements();
+    }
+
+    createDOMElements(){
+        const content = document.querySelector('#content');
+        content.innerHTML = `Hi`;
+    }
 }
