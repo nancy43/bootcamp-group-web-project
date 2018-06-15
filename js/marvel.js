8d378121bd4ba439548d4606034e6e274a1034a7
Manu <manu.panday5@gmail.com>
2018-06-15 18:49:30 -0400

    Connecting to api

--- js/marvel.js
+++ js/marvel.js
@@ -17,4 +17,24 @@ class character{
   }
 
 }
+// to handle event 
+find = (event) =>{
+event.preventDefault();
+var param= document.getElementById("name").value;
+var marvelUrl="https://gateway.marvel.com/v1/public/characters?name="+param+"&ts=1&apikey=72469e3c574bb2c9b801a5a820cb1544&hash=c3943e7f8e5b58c6d62d81b7393fe03b";
+
+
+$.ajax({
+      url: marvelUrl,
+      dataType:'json',
+    }).then(resultAPI => {
+     console.log(resultAPI);
+     alert(resultAPI.data.results[0].name);
+    }).catch( e  => console.log(e));
+
+};
+document.getElementById("marvelform").addEventListener('submit',find); 
+
+
+
 
