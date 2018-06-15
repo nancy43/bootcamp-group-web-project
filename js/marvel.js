9181f8f196ce5066f2f4eb1901784fefab0937a6
Manu <manu.panday5@gmail.com>
2018-06-15 17:50:39 -0400

    Created form to submit name of the marvel character. create class for the character in js

---
+++ js/marvel.js
@@ -0,0 +1,20 @@
+class character{
+	constructor(name,description,image){
+  	this.name=name;
+    this.description=description;
+    this.image=image;
+  }
+  //getName is method , name is variable
+  getName(){
+    return this.name;
+    
+  }
+  getDescription(){
+  	return this.description;
+  }
+  getImage(){
+   	return this.image;
+  }
+
+}
+
