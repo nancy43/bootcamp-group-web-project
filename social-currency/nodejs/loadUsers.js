45e9a2e0e23b1453ce05c36279bf6cf327b4ef87
Bruno Santos <ebrunosan@gmail.com>
2018-06-23 12:48:38 -0400

    Include seed and new field badge

--- social-currency/nodejs/loadUsers.js
+++ social-currency/nodejs/loadUsers.js
@@ -16,7 +16,7 @@ admin.initializeApp({
 /*--
     It gets user's json object from API based on:
 		(1) 2 countries Canada and Brazil
-		(2) get 50 random users
+		(2) get 6 random users
 		(3) retrieve only name, picture, and country
 		
 		Reference: https://randomuser.me/documentation
@@ -24,7 +24,7 @@ admin.initializeApp({
 let initApp = function() {
     clearUsersLogin();
 
-	const url = 'https://randomuser.me/api/?results=50&nat=au,ca,ch,br,us&inc=login,name,email,picture,nat,phone';
+	const url = 'https://randomuser.me/api/?seed=foo&results=50&nat=au,ca,ch,br,us&inc=login,name,email,picture,nat,phone';
 
 	fetch(url)
 		.then( response => {
@@ -116,6 +116,12 @@ let getRandomVal = function(nat) {
     return 500 * getRndInteger(1,3);
 };
 
+// returns random values: 500 or 1000 or 1500
+let getRandomBadge = function(nat) {
+    let num = getRndInteger(1,10);
+    return (num < 5? 0 : num);
+};
+
 let addUserToFirebase = function(users) {
     users.map( user => {
         admin.auth().createUser({
@@ -127,22 +133,28 @@ let addUserToFirebase = function(users) {
             emailVerified:  true,
             disabled:       false
     
-            }).then(function(userRecord) {
+            }).then(function( userRecord ) {
+                let userNat = getFormattedNat(user.nat);
+            
+                let userObj = {
+                    'displayName'   : formatName(user.name.first, user.name.last),
+                    'photoURL'      : user.picture.large,
+                    'email'         : user.email,
+                    'phone'         : user.phone,
+                    'nat_withdraw'  : userNat,
+                    'tot_withdraw'  : getRandomVal(),
+                    'nat_deposit'   : getRndCountry(userNat),
+                    'badge'         : getRandomBadge(),
+                };
+                
+                admin.database().ref('users/' + user.login.username).set(userObj);
+
                 console.log("Successfully created new user:", user.login.username);
 
             }).catch(function(error) {
                 console.log("Error creating new user:", user.login.username);
-            })
-            
-            let userObj = {
-                'phone'         : user.phone,
-                'nat_withdraw'  : getFormattedNat(user.nat),
-                'tot_withdraw'  : getRandomVal(),
-                'nat_deposit'   : getRndCountry(user.nat),
-            };
-            admin.database().ref('users/' + user.login.username).set(userObj);
-        ; // END CreateUser
-    });
-};
+            }); // END createUser
+    }); // END map
+}; // END function
 
 initApp();
