983b20f7e88f7bd53c23fbb07fc0b6bcd6b014fe
Nancy Sharma <>
2018-06-24 02:31:25 -0400

    some changes

--- js/marvel.js
+++ js/marvel.js
@@ -2,7 +2,7 @@ var marvel = {
 	render: function() {
 		var url = "http://gateway.marvel.com/v1/public/characters?ts=1&apikey=b89508034bfeab38dfa4d135c6c96e26&hash=d3a36f12dcdbe4c6fb5f01f854adba63"
 		var message = document.getElementById("message");
-        var footer = document.getElementById("footer");
+    
 		var marvelContainer = document.getElementById("marvel-container"); 
 		
 	  $.ajax({
@@ -28,7 +28,7 @@ var marvel = {
 				string += "</a>";
 				string += "<h3>" +element.name + "</h3> "; 
 				string += "</div>";
-				if((i+1) % 4 == 0) {   
+				if((i+1) % 5 == 1) {   
 		 string += "<div>";
 		 string += "<div class='row'>";
 		 
